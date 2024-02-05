c79387ddf0039c3fe3cba1b30ad59d600ad15eed
unknown <srikanthcollaty92@gmail.com>
2024-02-05 15:42:17 +0100

    first commit

---
+++ src/script.js
@@ -0,0 +1,157 @@
+var App = window.App || {};
+
+/*  ===============================
+    PROJECTS
+    =============================== */
+
+    // Model
+    App.Project = Backbone.Model.extend();
+
+    // Collection
+    App.Projects = Backbone.Model.extend({
+        model : App.Project
+    });
+
+	// List of projects
+    var Projects = new App.Projects([
+        { title : "Lorem ipsum", visual : "http://lorempicsum.com/futurama/200/200/1", link : "http://www.google.fr/"},
+        { title : "Dolor sit amet", visual : "http://lorempicsum.com/futurama/200/200/2", link : "http://www.google.fr/"},
+        { title : "Consectetur adipisicing", visual : "http://lorempicsum.com/futurama/200/200/3", link : "http://www.google.fr/"},
+        { title : "Cum neque", visual : "http://lorempicsum.com/futurama/200/200/4", link : "http://www.google.fr/"},
+        { title : "Modi non molestiae", visual : "http://lorempicsum.com/futurama/200/200/5", link : "http://www.google.fr/"},
+        { title : "Soluta laboriosam", visual : "http://lorempicsum.com/futurama/200/200/6", link : "http://www.google.fr/"}
+    ]);
+
+/*  ===============================
+    VIEWS
+    =============================== */
+
+    // Home
+    App.homeView = Backbone.View.extend({
+
+        el : "#mainContent",
+
+        initialize: function(){
+            console.log("Home");
+            this.template = _.template($('#home').html());
+        },
+
+        render : function(){
+            this.$el.html(this.template);
+            return true;
+        }
+    });
+
+    // About
+    App.aboutView = Backbone.View.extend({
+
+        el : "#mainContent",
+
+        initialize : function(){
+            console.log("About");
+            this.template = _.template($('#about').html());
+        },
+
+        render: function(){
+            this.$el.html(this.template);
+            return true;
+        }
+    });
+
+    // Projects
+    App.projectsView = Backbone.View.extend({
+
+        el : "#mainContent",
+
+        initialize : function(){
+            console.log("Projects");
+            this.template = _.template($('#projects').html());
+        },
+
+        render : function(){
+            this.$el.html( this.template({ projects: this.collection.toJSON() }) );
+            return true;
+        }
+    });
+
+    // Contact
+    App.contactView = Backbone.View.extend({
+
+        el : "#mainContent",
+
+        initialize : function(){
+            console.log("Contact");
+            this.template = _.template($('#contact').html());
+        },
+
+        render : function(){
+            this.$el.html(this.template);
+        }
+    });
+
+/*  ===============================
+    ROUTES
+    =============================== */
+
+    App.Routes = Backbone.Router.extend({
+
+        routes : {
+            '':          "home",
+            "home":      "home",
+            "about":     "about",
+            "projects":     "projects",
+            "contact":     "contact"
+        },
+
+        home : function(){
+            var homeView = new App.homeView();
+            homeView.render();
+        },
+
+        about : function(){
+            var aboutView = new App.aboutView();
+            aboutView.render();
+        },
+
+        projects : function(){
+            var projectsView = new App.projectsView({ collection : Projects });
+            projectsView.render();
+        },
+
+        contact : function(){
+            var contactView = new App.contactView();
+            contactView.render();
+        }
+    });
+
+    var myRoutes = new App.Routes();
+    Backbone.history.start();
+
+/*  ===============================
+    MENU
+    =============================== */
+
+    var menuActiveHandler = function(){
+        // On menu click
+        $('.mainMenu li a').on('click', function(){
+            $('.mainMenu li').removeClass();
+            $(this).parent().addClass('active');
+        });
+    }
+
+    var menuActiveLocationHandler = function(){
+        var currentPageName = location.hash;
+        $('.mainMenu li').removeClass();
+        currentPageName ?
+            $('.mainMenu li a[href="'+ currentPageName +'"]').parent().addClass('active') :
+            $('.mainMenu li a[href="#home"]').parent().addClass('active');
+    }
+
+    // On page load
+    menuActiveHandler();
+    menuActiveLocationHandler();
+
+    // On location change
+    $(window).bind('hashchange', function() {
+        menuActiveLocationHandler();
+    });
